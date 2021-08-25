8853d205bf3aacc7e5fb25382cccf354e7e59f1b
Gowtham Chandrasekar <gowthamtrm01@gamil.com>
2021-08-25 14:04:32 +0530

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+
+import { listProducts } from '../actions/productActions';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+
+function HomeScreen() {
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const { error, loading, products } = productList;
+
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch])
+
+    return(
+        <div>
+            <h2>Latest Products</h2>
+            { loading ? <Loader />
+             : error ? <Message variant='danger'>{ error }</Message>
+                :   <Row>
+                        { products.map((product) => {
+                            return (
+                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product}/>
+                                </Col>
+                            )
+                        }) }
+                    </Row>
+            }
+        </div>
+    )
+}
+
+export default HomeScreen;
